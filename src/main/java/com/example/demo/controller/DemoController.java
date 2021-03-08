27e82de260faa3103a5f23cbbd1d56d784026200
Chanaka Dilshan <chanu619@gmail.com>
2021-03-09 11:04:08 +1300

    added home url

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     @GetMapping
+    public String home() {
+        return "<h1>Spring Boot Hello World!</h1>";
+    }
+
+    @GetMapping(value = "/counter")
     public Boolean testCounter(@RequestParam(value ="id") int id) {
         return id == 10 ? true : false;
     }
